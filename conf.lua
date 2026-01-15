a592a6f5499ca545ce3d57daf6f45182a42d74ed
Sarner <arabic148813@gmail.com>
2026-01-15 23:25:40 +0400

    Add LOVE config (conf.lua)

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.identity = "lua-arcade"
+  t.window.title = "Lua Arcade (LÖVE)"
+  t.window.width = 1280
+  t.window.height = 720
+  t.window.resizable = true
+  t.window.vsync = 1
+end
